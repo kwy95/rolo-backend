810fce358d2e890444346cf6a0d23c9d2da6fc5c
Rubens Gomes Neto <rubens.gomes.neto@protonmail.com>
2025-01-14 15:47:51 -0300

    Listar portas serial disponíveis
    Encontra as portas disponíveis e lista as encontradas.
    
    Signed-off-by: Rubens Gomes Neto <rubens.gomes.neto@protonmail.com>

--- arduino/serial.go
+++ arduino/serial.go
@@ -1,7 +1,24 @@
 package arduino
 
-import "log"
+import (
+	"log"
+	"go.bug.st/serial"
+)
 
 func Start() {
 	log.Println("started")
+	getPorts()
+}
+
+func getPorts() {
+	ports, err := serial.GetPortsList()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(ports) == 0 {
+		log.Fatal("No serial ports found!")
+	}
+	for _, port := range ports {
+		log.Printf("Found port: %v\n", port)
+	}
 }
