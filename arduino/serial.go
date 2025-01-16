1e24947295fd249ec3008c9cc9f2ab14dfca7bbc
Rubens Gomes Neto <rubens.gomes.neto@protonmail.com>
2025-01-15 22:43:35 -0300

    Conectar com arduino por serial
    Realiza um "aperto de mão" para garantir que a conexão foi feita pela
    porta serial correta. Para a confirmação foi importante adicionar o
    flush e o sleep para garantir que o buffer não estivesse limpo e o sinal
    recebido fosse lido de forma direta.
    
    Signed-off-by: Rubens Gomes Neto <rubens.gomes.neto@protonmail.com>

--- arduino/serial.go
+++ arduino/serial.go
@@ -2,15 +2,22 @@ package arduino
 
 import (
 	"log"
+	"time"
 	"go.bug.st/serial"
 )
 
 func Start() {
 	log.Println("started")
-	getPorts()
+
+	ports := getPorts()
+	port := connectArduino(ports)
+	if port == nil {
+		log.Fatal("Failed to connect to Arduino")
+	}
+	defer port.Close()
 }
 
-func getPorts() {
+func getPorts() []string {
 	ports, err := serial.GetPortsList()
 	if err != nil {
 		log.Fatal(err)
@@ -18,7 +25,55 @@ func getPorts() {
 	if len(ports) == 0 {
 		log.Fatal("No serial ports found!")
 	}
-	for _, port := range ports {
-		log.Printf("Found port: %v\n", port)
+	return ports
+}
+
+func connectArduino(ports []string) serial.Port {
+	mode := &serial.Mode{
+		BaudRate: 9600,
 	}
+
+	for _, port_name := range ports {
+		port, err := serial.Open(port_name, mode)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if !confirmArduino(port) {
+			port.Close()
+			continue
+		}
+
+		return port
+	}
+
+	return nil
+}
+
+func confirmArduino(port serial.Port) bool {
+	port.ResetInputBuffer()
+	time.Sleep(1)
+	buff := make([]byte, 1)
+	n, err := port.Read(buff)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if n == 0 {
+		return false
+	}
+
+	if string(buff[:n]) == "A" {
+		n, err = port.Write([]byte("A"))
+		if err != nil {
+			log.Fatal(err)
+		}
+		if n == 0 {
+			log.Fatal("Failed to write to Arduino")
+		}
+		log.Printf("sent %v bytes to arduino\n", n)
+		port.ResetInputBuffer()
+		return true
+	}
+
+	return false
 }
