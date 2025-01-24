8f9a2006dcb8eba47203c0d30b01da6288172f50
Rubens Gomes Neto <rubens.gomes.neto@protonmail.com>
2025-01-24 03:19:52 -0300

    Cria WS manager
    Busquei de um exemplo e fiz muitas coisas sem testar, será importante
    testar essa implementação depois.
    
    Signed-off-by: Rubens Gomes Neto <rubens.gomes.neto@protonmail.com>

---
+++ api/websocket_manager.go
@@ -0,0 +1,81 @@
+package api
+
+import (
+	"github.com/gobwas/ws"
+	"github.com/gobwas/ws/wsutil"
+	"log"
+	"net"
+	"sync"
+)
+
+type WSManager struct {
+	live   *liveSocket
+	buffer chan []byte
+	lock   sync.Mutex
+}
+
+type liveSocket struct {
+	conn net.Conn
+}
+
+func NewWSManager(buffer chan []byte) *WSManager {
+	m := &WSManager{
+		live:   nil,
+		buffer: buffer,
+		lock:   sync.Mutex{},
+	}
+
+	return m
+}
+
+func (w *WSManager) RegisterWSConnection(ws net.Conn) {
+	log.Println("new WS registration")
+	newLive := &liveSocket{
+		conn: ws,
+	}
+	w.lock.Lock()
+	w.live = newLive
+	w.lock.Unlock()
+
+	go w.listenForDataFromWSUntilDead()
+	go w.sendBuffer()
+}
+
+func (w *WSManager) listenForDataFromWSUntilDead() {
+	for {
+		msg, op, err := wsutil.ReadClientData(w.live.conn)
+		if err != nil {
+			log.Println("dead WS, exiting read loop ", msg, op, err)
+			w.lock.Lock()
+			w.live.conn.Close()
+			w.live = nil
+			w.lock.Unlock()
+			break
+		}
+		log.Println("ws data: ", msg, op)
+	}
+}
+
+func (w *WSManager) sendBuffer() {
+	for w.live != nil {
+		err := w.SendToSocket(<-w.buffer)
+		if err != nil {
+			log.Println("dead WS, exiting send loop ", err)
+			w.lock.Lock()
+			w.live.conn.Close()
+			w.live = nil
+			w.lock.Unlock()
+			break
+		}
+	}
+}
+
+func (w *WSManager) SendToSocket(msg []byte) error {
+	err := wsutil.WriteServerMessage(w.live.conn, ws.OpText, msg)
+
+	if err != nil {
+		log.Println("error writing to WS: ", err, msg)
+	}
+
+	return err
+}
